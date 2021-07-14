853a315ee5974982dbc079a9747836fa5b6450bf
franguyarmani <fbpeabdy@gmail.com>
2021-07-13 20:59:34 -0400

    add a bunch of random endpoints from the feed

--- api/app.py
+++ api/app.py
@@ -1,8 +1,28 @@
 import time
+import feedparser
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+feed_url = "http://feeds.marketwatch.com/marketwatch/marketpulse/.rss"
+
+@app.route('/title')
+def get_title():
+    title = feedparser.parse(feed_url).feed.title
+    return {'title': title}
+
+@app.route('/feed')
+def get_feed():
+    feed = feedparser.parse(feed_url)
+    return {'feed': feed.feed}
+
+@app.route('/links')
+def get_links():
+    feed = feedparser.parse(feed_url).feed
+    return {'links': feed.links}
+
+@app.route('/summaries')
+def get_summaries():
+    feed = feedparser.parse(feed_url)
+    summaries = [x.summary for x in feed.entries]
+    return {'summaries': summaries}
